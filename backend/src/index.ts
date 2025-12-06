e556add859c65a25b1644b736040e837c71e9256
michael-y-ackerman <michaelackerman314@gmail.com>
2025-12-06 01:50:18 -0500

    fixed cors issue in API, updated gitignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,15 +3,24 @@ dotenv.config();
 
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const PORT = process.env.PORT || 5001; 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);  
